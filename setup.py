db3df6106511e3fb9ddb55d51c061842a1ccf36b
Kenneth Miller <xkenneth@TheSamaritan.local>
2009-04-21 11:54:31 -0500

    lots of reorganizing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "PyDecode",
+    version = "1.1.3",
+    description='Standard teledrill python library',
+    author='Kenneth Miller',
+    author_email='kenneth.miller@teledrill.com',
+    url='http://www.teledrill.com',
+    packages = find_packages(),
+)
